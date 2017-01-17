41b6228f98cb3f4a0ac36aebf07d5d82f9c1ceb5
Zee <amurphey7@gmail.com>
2017-01-16 20:43:55 -0500

    Added invsee command, may be updating this soon with custom inventory title

--- src/me/zee/mcadministration/MCAdministration.java
+++ src/me/zee/mcadministration/MCAdministration.java
@@ -60,6 +60,7 @@ public class MCAdministration extends JavaPlugin implements Listener {
 		getCommand("tempban").setExecutor(new CMD_TempBan(this));
 		getCommand("unban").setExecutor(new CMD_Unban(this));
 		getCommand("announce").setExecutor(new CMD_Announce(this));
+		getCommand("invsee").setExecutor(new CMD_Invsee(this));
 	}
 	
 	/**
